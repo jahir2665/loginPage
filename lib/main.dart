f8577fddcd94a86e812681fa25ee66d3bb592e2b
jahir2665 <jahir37.csecu@gmail.com>
2025-02-18 23:24:02 +0600

    version 1

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  return MaterialApp(home: HomeActivity(),debugShowCheckedModeBanner: false);
+
+  }
+
+}
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text("Login page"),
+    backgroundColor: Colors.cyan,),
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          'Login',
+          style: TextStyle(
+            fontSize: 35,
+            color: Colors.teal,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: Form(
+              child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.all(15),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),
+                      labelText: 'Email',prefixIcon:Icon(Icons.email) ),),),
+                  Padding(padding: EdgeInsets.all(15),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),
+                      labelText: 'phone number',prefixIcon:Icon(Icons.phone) ),),),
+                  Padding(padding: EdgeInsets.all(15),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),
+                      labelText: 'password',prefixIcon:Icon(Icons.password) ),),),
+
+                  SizedBox(height: 30,),
+
+                  MaterialButton(onPressed: (){},
+                  child:Text('Login'),
+                    color: Colors.green,
+                    textColor: Colors.white,
+                  ),
+                ],
+              )
+          ),
+        ),
+      ],
+    ),
+  );
+  }
+}
